bdc82d8ce11e7aad79e5e5f5bcc3c5e48a33b2f4
zdxhf <zdxhf@163.com>
2015-01-19 17:49:16 +0800

    new update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,6 +16,25 @@ namespace hotsAPI.Controllers
         {
             return View();
         }
+        public ActionResult AddRecords()
+        {
+            List<SelectListItem> fromList = new List<SelectListItem>();
+            fromList.Add(new SelectListItem { Text = "风暴英雄", Value = "风暴英雄" });
+            fromList.Add(new SelectListItem { Text = "魔兽世界", Value = "魔兽世界" });
+            fromList.Add(new SelectListItem { Text = "炉石传说", Value = "炉石传说" });
+            fromList.Add(new SelectListItem { Text = "星际争霸", Value = "星际争霸" });
+            fromList.Add(new SelectListItem { Text = "魔兽争霸", Value = "魔兽争霸" });
+            fromList.Add(new SelectListItem { Text = "其他相关", Value = "其他相关" });
+            ViewData["FromList"] = new SelectList(fromList, "Value", "Text", "");
+
+            List<SelectListItem> typeList = new List<SelectListItem>();
+            typeList.Add(new SelectListItem { Text = "蓝贴发布", Value = "蓝贴发布" });
+            typeList.Add(new SelectListItem { Text = "前瞻咨询", Value = "前瞻咨询" });
+            typeList.Add(new SelectListItem { Text = "数据统计", Value = "数据统计" });
+            typeList.Add(new SelectListItem { Text = "其他相关", Value = "其他相关" });
+            ViewData["TypeList"] = new SelectList(typeList, "Value", "Text", "");
+            return View();
+        }
         //[HttpPost]
         //public ActionResult Index(hotsAPI.Models.Hero hreo)
         //{
---
+++ Models/GameNews.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+
+namespace hotsAPI.Models
+{
+    public class GameNews
+    {
+        public int ID { get; set; }
+        public string Date { get; set; }
+
+        [Required]
+        [DataMember(IsRequired = true)]
+        [DisplayName("游戏分类")]
+        public string From { get; set; }
+
+        [Required]
+        [DataMember(IsRequired = true)]
+        [DisplayName("原帖链接")]
+        public string Link { get; set; }
+
+        [Required]
+        [DataMember(IsRequired = true)]
+        [DisplayName("资讯类型")]
+        public string Type { get; set; }
+        
+        [Required]
+        [DataMember(IsRequired = true)]
+        [DisplayName("主题")]
+        public string Title { get; set; }
+    }
+}
